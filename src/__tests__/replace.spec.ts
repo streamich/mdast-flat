d138cee8eca1ab47d1eea9cbfc626731ea59bce9
streamich <streamich@gmail.com>
2019-02-25 22:36:23 +0100

    docs: ✏️ add replace() function reference to README

--- src/__tests__/replace.spec.ts
+++ src/__tests__/replace.spec.ts
@@ -7,6 +7,31 @@ describe('structure', () => {
     expect(typeof replace).toBe('function');
   });
 
+  it('simple example', () => {
+    const parser = create();
+    const mdast1 = parser.tokenizeBlock('1\n' + '\n' + 'replace me\n');
+    const mdast2 = parser.tokenizeBlock('2\n');
+    const flat1 = mdastToFlat(mdast1!);
+    const flat2 = mdastToFlat(mdast2!);
+    const merged = replace(flat1, 3, flat2);
+
+    expect(merged).toMatchObject({
+      nodes: [
+        {type: 'root', children: [1, 3], idx: 0},
+        {type: 'paragraph', children: [2], idx: 1},
+        {type: 'text', value: '1', idx: 2},
+        {type: 'portal', idx: 3, original: {type: 'paragraph', children: [4], idx: 3}, children: [5]},
+        {type: 'text', value: 'replace me', idx: 4},
+        {type: 'root', children: [6], idx: 5},
+        {type: 'paragraph', children: [7], idx: 6},
+        {type: 'text', value: '2', idx: 7},
+      ],
+      contents: [],
+      definitions: {},
+      footnotes: {},
+    });
+  });
+
   it('merges nodes', () => {
     const parser = create();
     const mdast1 = parser.tokenizeBlock('1\n' + '\n' + 'here\n' + '\n' + '4\n');
